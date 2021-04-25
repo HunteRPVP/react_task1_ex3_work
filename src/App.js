ca73dde34462557d1c171c4cb665329332ebe41e
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-04-25 12:09:31 +0300

    added antd and datepicker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { DatePicker, message } from 'antd';
+import React, { useState } from 'react';
 
 function App() {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>
+          Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        </div>
+      </div>
     </div>
   );
 }
